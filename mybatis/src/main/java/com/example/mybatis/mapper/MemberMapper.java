69c671d1e0442ff44fd86bc39d7058aa8dbbc22b
Heesun Kim <wonderful.coding.life@gmail.com>
2024-11-26 11:03:04 +0900

    added xml based mapper with mybatis

---
+++ mybatis/src/main/java/com/example/mybatis/mapper/MemberMapper.java
@@ -0,0 +1,20 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.model.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface MemberMapper {
+    List<Member> findAll();
+    Member findById(@Param("id") Long id);
+    Optional<Member> findByEmail(@Param("email") String email);
+    List<Member> findAllOrderByAgeAsc();
+    int count();
+    int save(@Param("member") Member member);
+    int update(@Param("member") Member member);
+    int delete(@Param("id") Long id);
+}
---
+++ mybatis/src/test/java/com/example/mybatis/mapper/MemberMapperTests.java
@@ -0,0 +1,63 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.model.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@Slf4j
+@Sql(scripts = {"classpath:/test-member.sql"})
+public class MemberMapperTests {
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    public void findAll() {
+        List<Member> members = memberMapper.findAll();
+        for (Member member : members) {
+            log.info("{}", member);
+        }
+    }
+
+    @Test
+    public void findById() {
+        Member member = memberMapper.findById(3L);
+        log.info("{}", member);
+    }
+
+    @Test
+    public void findByEmail() {
+        Optional<Member> member = memberMapper.findByEmail("KwangcheolYoon@hanbit.co.kr");
+        log.info("{}", member.orElseThrow());
+    }
+
+    @Test
+    public void findAllOrderByAgeAsc() {
+        List<Member> members = memberMapper.findAllOrderByAgeAsc();
+        for (Member member : members) {
+            log.info("{}", member);
+        }
+    }
+
+    @Test
+    public void count() {
+        log.info("count= {}", memberMapper.count());
+    }
+
+    @Test
+    public void insert() {
+        Member member = Member.builder()
+                .name("홍길동")
+                .email("hong@hanbit.co.kr")
+                .age(12).build();
+        memberMapper.save(member);
+        log.info("count= {}", memberMapper.count());
+        log.info("count= {}", memberMapper.findById(member.getId()));
+    }
+}
