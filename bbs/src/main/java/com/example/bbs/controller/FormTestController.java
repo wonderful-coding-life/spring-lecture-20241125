0da8bfc85f7f315b87ed6ee34826bde79b154fe8
spiderman <spiderman@xhift.co.kr>
2024-11-29 11:55:08 +0900

    added form test

--- bbs/src/main/java/com/example/bbs/controller/FormTestController.java
+++ bbs/src/main/java/com/example/bbs/controller/FormTestController.java
@@ -5,6 +5,7 @@ import com.example.bbs.dto.TestFormValidation;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -39,18 +40,29 @@ public class FormTestController {
 
     @GetMapping("/c")
     public String getC(@ModelAttribute("testForm") TestForm testForm) {
+        testForm.setName("홍길동...");
+        testForm.setAddress("서울시...");
         return "test-form-c";
     }
 
     @PostMapping("/c")
     public String postC(@ModelAttribute("testForm") TestForm testForm) {
         log.info("post c {}", testForm);
+
+        boolean hasError = false;
         if (testForm.getName().isBlank()) {
-            return "test-form-c";
+            testForm.setName("이름을 넣으시라니까요");
+            hasError = true;
         }
         if (testForm.getAddress().isBlank()) {
-            return "test-form-c";
+            testForm.setAddress("주소는 꼭 입력해야 합니다");
+            hasError = true;
         }
+
+        if (hasError) {
+            return "/test-form-c";
+        }
+
         return "redirect:/";
     }
 
@@ -65,9 +77,11 @@ public class FormTestController {
         if (testForm.getName().equals("admin")) {
             bindingResult.rejectValue("name", "NotAllowed", "사용할 수 없는 이름입니다");
         }
+
         if (bindingResult.hasErrors()) {
             return "test-form-d";
         }
+
         return "redirect:/";
     }
 }
